b38ca3ee1c303bdd603db29bb3efede65d129dc0
Adam Risi <ajrisi@gmail.com>
2009-11-11 12:11:23 -0500

    adding some API details to header file

--- src/client_thread.c
+++ src/client_thread.c
@@ -6,30 +6,30 @@ void *client_thread(void *data)
 {
   http_request r;
   client_data *cd;
-  char buffer[MAX_REQUEST_LENGTH];
+  char buffer[MAX_REQUEST_HEADER_LENGTH];
   int rc;
   webpage_table_item *page;
 
   cd = (client_data*)data;
   memset(&r, 0, sizeof(r));
-
-
+  
   /* read from the remote machine to get the method and URL */
-  rc = read(cd->remote_fd, buffer, MAX_REQUEST_LENGTH); 
+  rc = read(cd->remote_fd, buffer, MAX_REQUEST_HEADER_LENGTH); 
   if(rc == -1) {
     /* was unable to read from the remote FD, so quit */
     return NULL;
   }
 
   /* parse the request */
-  rc = http_request_parse(&r, buffer, MAX_REQUEST_LENGTH);
+  rc = http_request_parse(&r, buffer, MAX_REQUEST_HEADER_LENGTH);
   if(rc < 0) {
     /* unable to parse the http request */
+    send_error(cd->remote_fd, 400, "Bad Request");
     return NULL;
   }
 
   printf("URL: %s\n", r.url);
-
+ 
   /* now, lookup the URL, and do the app. reply */
   page = pagetable_find(cd->ws->pages, r.url);
   if(page == NULL) {
@@ -42,7 +42,7 @@ void *client_thread(void *data)
       send_error(cd->remote_fd, 200, page->value.buffer);
       break;
     defualt:
-      send_error(cd->remote_fd, 404, "Post type not supported");
+      send_error(cd->remote_fd, 501, "Post Type not Implemented");
       break;
     }
   }
--- src/http.h
+++ src/http.h
@@ -5,7 +5,8 @@
 
 #define MAX_URL_LENGTH 1024
 #define MAX_METHOD_LENGTH 16
-#define MAX_REQUEST_LENGTH 2048
+#define MAX_REQUEST_HEADER_LENGTH 2048
+#define MAX_REPLY_HEADER_LENGTH 2048
 
 typedef enum http_method_e http_method;
 enum http_method_e {
@@ -20,6 +21,14 @@ struct http_request_s {
   char url[MAX_URL_LENGTH];
 };
 
+typedef struct http_reply_s http_reply;
+struct http_reply_s {
+  int code;
+  char *message;
+  unsigned int size;
+  char *mime;
+  
+};
 
 int http_request_parse(http_request *r, char *buffer, unsigned int blen);
 int send_error(int fd, int code, char *desc);
